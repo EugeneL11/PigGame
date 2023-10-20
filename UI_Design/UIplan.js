cc98c6b4e111563674c4fde65421566dd503f3ac
EugeneL11 <eugenelee0605@gmail.com>
2023-10-20 14:59:58 -0600

    add file descriptions and start README.md

---
+++ UI_Design/UIplan.js
@@ -0,0 +1,22 @@
+// Course: SENG 513
+// Date: OCT 20, 2023
+// Assignment 2
+// Name: Eugene Lee
+// UCID: 30137489 
+
+// Define required global variables
+
+// Function to initialize the game
+function initializeGame() {
+    // Initialize game state and settings, e.g., player names, target score, forbidden number, scores, etc.
+    // Display the game rules pop-up
+    // Update the UI to reflect initial game state
+}
+
+// Function to roll the dice
+function rollDice() {
+    // Generate a random dice roll result (1 to 6) rand = rand(1,6)
+    // Update the UI to display the roll result
+    // If the roll result is equal to the forbidden number, reset turnTotal to 0, end the turn, and switch players
+    // Otherwise, add the roll result to turnTotal and update the UI
+}
